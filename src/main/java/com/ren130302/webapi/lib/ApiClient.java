d4512b53b3c3f4988c47b12eee4a82cd65fe0176
ren130302 <ren130302@DESKTOP-FPKN44H>
2022-12-11 22:41:59 +0900

    v1.4.12

--- src/main/java/com/ren130302/webapi/lib/ApiClient.java
+++ src/main/java/com/ren130302/webapi/lib/ApiClient.java
@@ -2,11 +2,11 @@ package com.ren130302.webapi.lib;
 
 import com.ren130302.webapi.lib.interfaces.IApiClient;
 
-import lombok.Data;
 import lombok.NonNull;
+import lombok.Value;
 import lombok.experimental.Accessors;
 
-@Data(staticConstructor = "set")
+@Value(staticConstructor = "set")
 @Accessors(fluent = true)
 public final class ApiClient
 	implements IApiClient {
